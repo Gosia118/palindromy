72838f21dfcd5806e819c6fbb21e7ec88683f44e
Gosia118 <98267760+Gosia118@users.noreply.github.com>
2026-02-10 21:19:04 +0100

    Update palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -3,7 +3,7 @@
 def my_word(word):
     my_word_1 = word[0:]
     my_word_2 = word[::-1]
-    return my_word_1 == my_word_2:
+    return my_word_1 == my_word_2
     
 my_word("potop")
 print(my_word("potop"))
