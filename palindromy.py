bd370c41482d0cb8474fed60fcee51c8b958b2af
Gosia118 <98267760+Gosia118@users.noreply.github.com>
2026-02-10 21:15:41 +0100

    Update function palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -3,11 +3,7 @@
 def my_word(word):
     my_word_1 = word[0:]
     my_word_2 = word[::-1]
-    if my_word_1 == my_word_2:
-        return True
-    else:
-        return False
+    return my_word_1 == my_word_2:
     
-
 my_word("potop")
 print(my_word("potop"))
